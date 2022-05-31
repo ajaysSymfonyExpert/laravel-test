e90d9309f5cd019444f0c70bedec5461c2f52ffd
Ajay Singh <ajay@planet-ride.com>
2022-05-31 23:22:01 +0530

    fixed migration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,8 +23,9 @@ class AuthServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    {Schema::defaultStringLength(191);
         $this->registerPolicies();
+        
 
         //
     }
